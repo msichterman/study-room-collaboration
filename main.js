45012b1ba2d6201196f8ad8adbaddc97e9c2da91
Matt Sichterman <msichterman1@gmail.com>
2019-10-27 22:20:02 -0500

    Subject input capitalization added

--- main.js
+++ main.js
@@ -153,3 +153,32 @@ window.addEventListener('DOMContentLoaded', event => {
   }
 });
 // -------------------------------------------------------------------------
+
+// UTILITIES----------------------------------------------------------------
+// Ensure that the 'Subject' input filter takes uppercase letters
+function forceKeyPressUppercase(e) {
+  var charInput = e.keyCode;
+  if (charInput >= 97 && charInput <= 122) {
+    // lowercase
+    if (!e.ctrlKey && !e.metaKey && !e.altKey) {
+      // no modifier key
+      var newChar = charInput - 32;
+      var start = e.target.selectionStart;
+      var end = e.target.selectionEnd;
+      e.target.value =
+        e.target.value.substring(0, start) +
+        String.fromCharCode(newChar) +
+        e.target.value.substring(end);
+      e.target.setSelectionRange(start + 1, start + 1);
+      e.preventDefault();
+    }
+  }
+}
+
+window.addEventListener('DOMContentLoaded', event => {
+  document
+    .getElementById('subject')
+    .addEventListener('keypress', forceKeyPressUppercase, false);
+});
+
+// -------------------------------------------------------------------------
